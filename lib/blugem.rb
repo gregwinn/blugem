0ad37aaa23f30208f8be960107ad10891b97e923
Greg Winn <greg@winn.ws>
2015-09-03 23:39:42 -0500

    Auth class added.

--- lib/blugem.rb
+++ lib/blugem.rb
@@ -1,9 +1,33 @@
-class Blugem
-  def self.test
-    return "test"
+require 'rest-client'
+require 'yaml'
+
+module Blugem
+  GEM_ROOT = File.expand_path("../..", __FILE__)
+  AUTH_INFO = YAML.load_file("#{GEM_ROOT}/config/blugem.yml")
+
+  # => BluZone Authentication
+  # BluZone still uses username/password auth
+  class Auth
+    # => Blugem::Auth.[method]
+    @@API_URL = "https://bluzone.io/portal/"
+
+    def self.login
+      r = RestClient.post "#{@@API_URL}auth/slogin",
+        {:username => AUTH_INFO["username"], :password => AUTH_INFO["password"]}.to_json,
+        :content_type => :json,
+        :accept => :json
+      @authToken = JSON.parse(r.body)["authToken"]
+      return @authToken
+    end
+
+    def self.test
+      # => Return authToken
+      return @authToken
+    end
+
+    def self.version
+      return Gem::VERSION
+    end
   end
 
-  def self.version
-    return Gem::VERSION
-  end
 end
--- spec/blugem_spec.rb
+++ spec/blugem_spec.rb
@@ -1,15 +1,21 @@
 #require 'spec_helper'
 require "./lib/blugem.rb"
 describe Blugem do
+	context "Authentication" do
+		it "should return and authToken" do
+			# => Placeholder test for now until Auth is finished
+			expect(Blugem::Auth.login).not_to be_empty
+		end
+	end
 
 	context "Gem version" do
     it "should be test" do
-      expect(Blugem.test).to include("test")
+      expect(Blugem::Auth.test).to include("test")
       # => Fail
     end
 
 		it "should not be empty" do
-			gemversion = Blugem.version
+			gemversion = Blugem::Auth.version
 			expect(gemversion).not_to be_empty
 		end
 	end
